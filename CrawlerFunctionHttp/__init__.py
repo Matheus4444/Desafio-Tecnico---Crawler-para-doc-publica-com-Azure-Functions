959702cd72c6210deb03d496b388d79e99f7d54b
Matheus Oliveira <mofaria9@gmail.com>
2025-02-14 11:43:59 -0300

    HTTPTrigger full e incremental unidos em um único arquivo, diferenciados por parâmetro de data.

--- CrawlerFunctionHttp/__init__.py
+++ CrawlerFunctionHttp/__init__.py
@@ -1,58 +1,11 @@
 import logging
-import os
 import azure.functions as func
-import re
 from atlassian import Confluence
+from .httpCrawler import crawl_confluence
+from datetime import datetime
 
 # Configure logging
 logging.basicConfig(level=logging.INFO)
-
-def sanitize_filename(filename):
-    return re.sub(r'[<>:"/\\|?*]', '_', filename)
-
-def save_html(content, directory, file_name):
-    try:
-        # Save the HTML content to a file
-        with open(file_name, 'w', encoding='utf-8') as file:
-            file.write(content)
-        logging.info(f'Documentation saved to {file_name}.')
-    except OSError as e:
-        logging.error(f'Error creating directory or writing file: {str(e)}')
-
-def fetch_confluence_page(confluence, page_id):
-    try:
-        page = confluence.get_page_by_id(page_id, expand='body.storage')
-        return page['body']['storage']['value']
-    except Exception as e:
-        logging.error(f'Error fetching page content for ID {page_id}: {str(e)}')
-        return None
-
-def get_page_title(confluence, page_id):
-    try:
-        page = confluence.get_page_by_id(page_id, expand='title')
-        return page['title']
-    except Exception as e:
-        logging.error(f'Error fetching page title for ID {page_id}: {str(e)}')
-        return 'index'
-
-def crawl_confluence(confluence, page_id, base_directory):
-    title = get_page_title(confluence, page_id)
-    sanitized_title = sanitize_filename(title)
-    
-    # Create directory structure based on page title
-    directory = os.path.join(base_directory, sanitized_title)
-    if not os.path.exists(directory):
-        os.makedirs(directory, exist_ok=True)
-    file_name = os.path.join(directory, f"{sanitized_title}.html")
-    
-    content = fetch_confluence_page(confluence, page_id)
-    if content:
-        save_html(content, directory, file_name)
-    
-    # Recursively crawl child pages
-    child_pages = confluence.get_child_pages(page_id)
-    for child_page in child_pages:
-        crawl_confluence(confluence, child_page['id'], directory)
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('HTTP trigger function processed a request.')
@@ -60,6 +13,8 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     try:
         space_key = req.params.get('space_key')
         confluence_url = req.params.get('confluence_url')
+        last_modified_date_str = req.params.get('last_modified_date')
+        
         if not space_key or not confluence_url:
             return func.HttpResponse("Please provide a space key and Confluence URL.", status_code=400)
 
@@ -67,10 +22,14 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
             url=confluence_url
         )
 
+        last_modified_date = None
+        if last_modified_date_str:
+            last_modified_date = datetime.strptime(last_modified_date_str, '%Y-%m-%d').date()
+
         base_directory = 'collected_docs'
         root_pages = confluence.get_all_pages_from_space(space_key, start=0, limit=1000, expand='body.storage')
         for root_page in root_pages:
-            crawl_confluence(confluence, root_page['id'], base_directory)
+            crawl_confluence(confluence, root_page['id'], base_directory, last_modified_date)
 
         return func.HttpResponse(f'Documentation successfully crawled and saved.', status_code=200)
     except Exception as e:
